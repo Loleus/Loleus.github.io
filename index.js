18b7f3967d271939201bcea3719f374e7f4c3578
Loleus <loleus@gmail.com>
2022-12-02 05:33:27 +0100

    cleaning_up

--- index.js
+++ index.js
@@ -1,55 +1,49 @@
 customElements.define("my-repos", class extends HTMLElement {
-    constructor() {
-        super();
-        this.attachShadow({ mode: "open" });
-    }
-    static get observedAttributes() { return ["loading", "repos"]; }
-    get loading() {
-        return JSON.parse(this.getAttribute("loading"));
-    }
-    set loading(v) {
-        this.setAttribute("loading", JSON.stringify(v));
-    }
-    async fetchrepos(url) {
-        this.loading = true;
-        const response = await fetch(url, { mode: 'cors' });
-        const json = await response.json();
-        this.reps = json;
-        this.loading = false;
-        console.log(this.reps)
-    }
-    async connectedCallback() {
-        this.shadowRoot.addEventListener("click", (e) => {
-            const name = e.target.name;
-            if (this[name]) {
-                this[name]();
-            }
-        });
-        await this.fetchrepos("https://api.github.com/users/Loleus/repos");
-        console.log(this.shadowRoot)
-    }
-    disconnectedCallback() {
-    }
-    attributeChangedCallback(attrName, oldVal, newVal) {
-        this.render();
-    }
-    render() {
-        let i = 1;
-        if (this.loading) {
-            this.shadowRoot.innerHTML = `Loading...`;
-        } else {
-            this.shadowRoot.innerHTML = `
+  constructor() {
+    super();
+    this.attachShadow({ mode: "open" });
+  }
+  static get observedAttributes() { return ["loading", "repos"]; }
+  get loading() {
+    return JSON.parse(this.getAttribute("loading"));
+  }
+  set loading(v) {
+    this.setAttribute("loading", JSON.stringify(v));
+  }
+  async fetchrepos(url) {
+    this.loading = true;
+    const response = await fetch(url, { mode: 'cors' });
+    const json = await response.json();
+    this.reps = json;
+    this.loading = false;
+  }
+  async connectedCallback() {
+    this.shadowRoot.addEventListener("click", (e) => {
+      console.log(e.target)
+    });
+    await this.fetchrepos("https://api.github.com/users/Loleus/repos");
+  }
+  disconnectedCallback() {
+  }
+  attributeChangedCallback(attrName, oldVal, newVal) {
+    this.render();
+  }
+  render() {
+    let i = 1;
+    if (this.loading) {
+      this.shadowRoot.innerHTML = `Loading...`;
+    } else {
+      this.shadowRoot.innerHTML = `
         <style>
         @import "table.css";
         </style>
-
         <span class="span">
           <h1><slot name="title">
-            <a style="text-underline-position: under;color:#00cc8991;" href="mailto:07zglossie@wp.pl?subject=aboutCode">
+            <a class="link" style="color:inherit;" href="mailto:07zglossie@wp.pl?subject=aboutCode">
               07zglossie@wp.pl
           </a></slot></h1>
           <table>
-            <tr style="color:grey;background:#1118">
+            <tr style="background:#1118">
               <th>No</th>
               <th>Name</th>
               <th>Type</th>
@@ -57,22 +51,21 @@ customElements.define("my-repos", class extends HTMLElement {
               <th>Language</th>
             </tr>
             ${this.reps.map(repo => {
-                if (repo.name != "loleus.github.io") {
-                    return `
+        if (repo.name != "loleus.github.io") {
+          return `
             <tr>
-              <td style="color:grey;background:#1118" >${i++}</td>
-              <td>${repo.name.toUpperCase()}</td>
-              <td style="color:grey">${repo.description}</td>
-              <td><a style="text-underline-position: under;color:gold;" target="_blank" href="https://loleus.github.io/${repo.name}">link</a></td>
-              <td>${repo.language}</td>
+              <td style="background:#1118" >${i++}</td>
+              <td style="color:var(--mainTxtCol)">${repo.name.toUpperCase()}</td>
+              <td>${repo.description}</td>
+              <td><a class="link" target="_blank" href="https://loleus.github.io/${repo.name}">link</a></td>
+              <td style="color:var(--mainTxtCol)">${repo.language}</td>
             </tr>
               `
-                }
-            }).join("")}
+        }
+      }).join("")}
           </table>
         </span>
-
       `;
-        }
     }
+  }
 });
